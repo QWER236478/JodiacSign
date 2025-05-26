352b78ba8ffeccea0d311e5c488d530ce394e217
QWER_236478 <acy021589@gmail.com>
2025-05-26 16:54:38 +0900

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnNewGameClick() //새로운 시작 버튼
+    {
+        SceneManager.LoadScene("SelectDifficulty"); // 난이도 선택 씬으로 이동
+    }
+
+    public void OnContinueClick() //이어하기 버튼
+    {
+        Debug.Log("이어하기");
+    }
+
+    public void OnSettingClick() //설정 버튼
+    {
+        Debug.Log("설정 창");
+    }
+
+    public void OnQuitClick() //종료하기 버튼
+    {
+        Application.Quit(); //종료
+        Debug.Log("종료됨");
+    }
+
+}
